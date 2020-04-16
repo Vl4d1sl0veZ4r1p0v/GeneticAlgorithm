04a53c51468347b66e858ef74f5257d65d19087d
Vladislav Zaripov <mr.vladislavzaripov@mail.ru>
2020-04-16 07:12:39 +0500

    start to fix graph

--- ConvertXML.py
+++ ConvertXML.py
@@ -1,12 +1,50 @@
-from pickle import loads
+from pickle import load
+import math
+from PIL import Image, ImageDraw
+import os
+from GeneticAlgorithm import Building, Room, Level
+
+
+class LevelContainer:
+
+    def __init__(self, population):
+        self.width = 70
+        self.height = 60
+        self.teil_size = 16
+        self.population = population
+        self.count_rooms = len(population[0].rooms)
+        #need for building
+        self.graph = []
+
+    def create_image(self, filename, idx):
+        img = Image.new("RGB", (self.width * self.teil_size, self.height * self.teil_size))
+        img1 = ImageDraw.Draw(img)
+        for room in self.population[idx].rooms:
+            x, y, w, l = map(lambda x: x * self.teil_size,
+                             (room.x, room.y, room.width, room._len))
+            shape = [(x, y), (x + w, y + l)]
+            img1.rectangle(shape, outline="#800080")
+        path = os.getcwd() + '/' + filename
+        img.save(path)
+
 
 class ConvertXML:
 
-    def __init__(self):
+    def __init__(self, out_tile=1, internal_tile=2, wall_tile=3):
+        self.out_tile = out_tile
+        self.internal_tile = internal_tile
+        self.wall_tile = wall_tile
+
+    def convert(self):
+        pass
+
 
 def main():
-    with open('first_generated.pkl', 'rb') as fin:
-
+    level = None
+    with open('offered_generated.pkl', 'rb') as fin:
+        level = load(fin)
+    test_container = LevelContainer(level)
+    test_container.create_image("offered_generation2.png", 1)
 
 if __name__ == "__main__":
     main()
--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -2,9 +2,14 @@ from random import choice, randint, seed, choices
 from pprint import pprint
 import numpy as np
 from copy import deepcopy
+from PIL import Image, ImageDraw
 import heapq
 from pickle import dump
-#seed(42)
+import pytest
+import os
+
+
+# seed(42)
 
 
 class Room:
@@ -25,10 +30,53 @@ class Room:
     def __repr__(self):
         return str((self.x, self.y, self._len, self.width, self._type))
 
+    # Graph evaluation
+
+    @staticmethod
+    def is_crossovered_by_axis(a1, b1, a2, b2):
+        a1, a2 = min(a1, a2), max(a1, a2)
+        b1, b2 = min(b1, b2), max(b1, b2)
+        a = max(a1, a2)
+        b = min(b1, b2)
+        return b - a >= 0
+
+    @staticmethod
+    def point_equality(a1, len1, a2, len2, a):
+        return a1 + a2 - 2 * a == len1 or a1 + a2 - 2 * a == len2
+
+    def has_common_dot(self, other_room):
+        assert isinstance(other_room, Room)
+        x = min(self.x, other_room.x)
+        y = min(self.y, other_room.y)
+        if self.point_equality(self.x, self.width,
+                               other_room.x, other_room.width, x) and \
+                self.point_equality(self.y, self._len,
+                                    other_room.y, other_room._len, y):
+            return True
+        return False
+
+    def is_crossovered(self, other_room):
+        assert isinstance(other_room, Room)
+        if self.is_crossovered_by_axis(self.x,
+                                       self.x + self.width,
+                                       other_room.x,
+                                       other_room.x + other_room.width) and \
+                self.is_crossovered_by_axis(self.y,
+                                            self.y + self._len,
+                                            other_room.y,
+                                            other_room.y + other_room._len):
+            if self.has_common_dot(other_room):
+                return False
+            return True
+        return False
+
 
 class Building:
 
     def __init__(self, rooms: list):
+        self.width = 70
+        self.height = 60
+        self.teil_size = 16
         self.rooms = rooms
         self.area = 0
         self.update_area()
@@ -38,6 +86,7 @@ class Building:
             self.maximize_total_rooms_area,
             self.minimize_total_rooms_area,
         ]
+        self.graph = []
 
     def __getitem__(self, index):
         return self.rooms[index]
@@ -82,15 +131,37 @@ class Building:
 
     # Graph evaluation(Deprecated)
 
+    def prepare_building(self):
+        self.graph = [set()] * len(self.rooms)
+        for i in range(len(self.rooms)):
+            for j in range(len(self.rooms)):
+                if i != j:
+                    if self.rooms[i].is_crossovered(self.rooms[j]):
+                        self.graph[i].add(j)
+
+    def create_image(self, filename):
+        img = Image.new("RGB", (self.width * self.teil_size, self.height * self.teil_size))
+        img1 = ImageDraw.Draw(img)
+        for idx, room in enumerate(self.rooms):
+            x, y, w, l = map(lambda x: x * self.teil_size,
+                             (room.x, room.y, room.width, room._len))
+            shape = [(x, y), (x + w, y + l)]
+            img1.rectangle(shape, outline="#800080")
+            img1.text((x + 5, y + 5), str(idx))
+        path = os.getcwd() + '/' + filename
+        img.save(path)
+
 
 class Level:
     types = ['o', 'u']
 
-    def __init__(self, n=1, rooms_count=1, max_coord=1, max_value=10, parents_count=3):
+    def __init__(self, n=1, rooms_count=1,
+                 max_coord=1, max_value=10, parents_count=3, min_value=6):
         self.population = Level.generate_population(n,
                                                     rooms_count,
                                                     max_coord,
-                                                    max_value)
+                                                    max_value,
+                                                    min_value)
         self.rooms_count = rooms_count
         self.n = n
         self.population_rank = []
@@ -123,10 +194,9 @@ class Level:
             heapq.heappush(self.population_rank, (building[0], i))
 
     def make_iteration(self):
-        current_building = None
         for i in range(self.n):
             current_building = deepcopy(self.population[i])
-            other = self.select_mating_pool(self.parents_count)#this parameter you can change
+            other = self.select_mating_pool(self.parents_count)  # this parameter you can change
             for parent in other:
                 if id(self.population[i]) != id(parent):
                     self.inherit_gene(parent, current_building)
@@ -136,30 +206,71 @@ class Level:
         self.kill_worst()
 
     @staticmethod
-    def generate_population(n, rooms_count, max_coord, max_value):
-        return [Building([Level.generate_item(max_coord, max_value)
+    def generate_population(n, rooms_count, max_coord, max_value, min_value):
+        return [Building([Level.generate_item(max_coord, max_value, min_value)
                           for _ in range(rooms_count)])
                 for _ in range(n)]
 
     @staticmethod
-    def generate_item(max_coord, max_value):
+    def generate_item(max_coord, max_value, min_value):
         x = randint(1, max_coord)
         y = randint(1, max_coord)
-        _len = randint(1, max_value)
-        width = randint(1, max_value)
+        _len = randint(min_value, max_value)
+        width = randint(min_value, max_value)
         _type = choice(Level.types)
         return Room(x, y, _len, width, _type)
 
-
+    def fit(self, rounds=1):
+        for _ in range(rounds):
+            self.make_iteration()
 
 
 def main():
-    test_level = Level(100, 10, 10, 15, 5)
-    for _ in range(100):
-        test_level.make_iteration()
-    with open('first_generated.pkl', 'wb') as fout:
-        dump(test_level.population, fout)
-    # pprint(test_level.population)
+    test_level = Level(n=5, rooms_count=10, max_coord=40, max_value=25, min_value=6)
+    test_level.fit(20)
+    test_level.population[0].prepare_building()
+    test_level.population[0].create_image("graph_implementing.png")
+    pprint(test_level.population[0].graph)
+    # with open('offered_generated.pkl', 'wb') as fout:
+    #     dump(test_level.population, fout)
+
+
+def test_is_crossovered_by_axis1():
+    a = Room(0, 0, 7, 7, 'o')
+    assert not a.is_crossovered_by_axis(0, 1, 2, 3)
+
+
+def test_is_crossovered_by_axis2():
+    a = Room(0, 0, 7, 7, 'o')
+    assert a.is_crossovered_by_axis(0, 1, 1, 3)
+
+
+def test_is_crossovered_by_axis3():
+    a = Room(0, 0, 7, 7, 'o')
+    assert a.is_crossovered_by_axis(0, 2, 1, 3)
+
+
+def test_is_crossovered_by_axis4():
+    a = Room(0, 0, 7, 7, 'o')
+    assert a.is_crossovered_by_axis(0, 1, 0, 3)
+
+
+def test_is_crossovered1():
+    a = Room(0, 0, 7, 7, 'o')
+    b = Room(0, 0, 7, 7, 'u')
+    assert a.is_crossovered(b)
+
+
+def test_is_crossovered2():
+    a = Room(0, 0, 7, 7, 'o')
+    b = Room(7, 7, 7, 7, 'u')
+    assert not a.is_crossovered(b)
+
+
+def test_is_crossovered3():
+    a = Room(0, 0, 7, 7, 'o')
+    b = Room(3, 3, 7, 7, 'u')
+    assert a.is_crossovered(b)
 
 
 if __name__ == "__main__":
